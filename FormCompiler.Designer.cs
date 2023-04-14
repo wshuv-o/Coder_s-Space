e9c365fde444ad231aa11b9f52eb688f6f064cd7
Imtiaj Sajin <imtiajsajin@gmail.com>
2023-04-14 11:35:10 +0600

    online compiler added

--- FormCompiler.Designer.cs
+++ FormCompiler.Designer.cs
@@ -33,129 +33,137 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.voteLabel = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.panelBrowser = new System.Windows.Forms.Panel();
             this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.panelBrowser = new System.Windows.Forms.Panel();
+            this.panel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.voteLabel = new System.Windows.Forms.Label();
             this.guna2Panel1.SuspendLayout();
-            this.panelBrowser.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.panelBrowser.SuspendLayout();
             this.SuspendLayout();
             // 
             // guna2Panel1
             // 
             this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
             this.guna2Panel1.BorderColor = System.Drawing.Color.White;
+            this.guna2Panel1.Controls.Add(this.voteLabel);
             this.guna2Panel1.Controls.Add(this.label4);
             this.guna2Panel1.Controls.Add(this.label3);
             this.guna2Panel1.Controls.Add(this.label2);
             this.guna2Panel1.Controls.Add(this.label1);
-            this.guna2Panel1.Controls.Add(this.voteLabel);
-            this.guna2Panel1.Controls.Add(this.label7);
             this.guna2Panel1.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.guna2Panel1.CustomizableEdges.BottomLeft = false;
             this.guna2Panel1.CustomizableEdges.TopRight = false;
-            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.guna2Panel1.Location = new System.Drawing.Point(1291, 0);
             this.guna2Panel1.Margin = new System.Windows.Forms.Padding(0, 3, 0, 3);
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.Padding = new System.Windows.Forms.Padding(1);
-            this.guna2Panel1.Size = new System.Drawing.Size(1350, 36);
+            this.guna2Panel1.Size = new System.Drawing.Size(59, 729);
             this.guna2Panel1.TabIndex = 2;
+            this.guna2Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2Panel1_Paint);
             // 
             // label4
             // 
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
-            this.label4.Location = new System.Drawing.Point(1036, 1);
+            this.label4.Location = new System.Drawing.Point(1, 103);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(27, 34);
+            this.label4.Size = new System.Drawing.Size(57, 34);
             this.label4.TabIndex = 6;
             this.label4.Text = "C";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label3
             // 
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
-            this.label3.Location = new System.Drawing.Point(1063, 1);
+            this.label3.Location = new System.Drawing.Point(1, 69);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 34);
+            this.label3.Size = new System.Drawing.Size(57, 34);
             this.label3.TabIndex = 5;
             this.label3.Text = "C++";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
             // 
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
-            this.label2.Location = new System.Drawing.Point(1118, 1);
+            this.label2.Location = new System.Drawing.Point(1, 35);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 34);
+            this.label2.Size = new System.Drawing.Size(57, 34);
             this.label2.TabIndex = 4;
             this.label2.Text = "PHP";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
-            this.label1.Location = new System.Drawing.Point(1170, 1);
+            this.label1.Location = new System.Drawing.Point(1, 1);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 34);
+            this.label1.Size = new System.Drawing.Size(57, 34);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Python";
-            // 
-            // voteLabel
-            // 
-            this.voteLabel.BackColor = System.Drawing.Color.Transparent;
-            this.voteLabel.Dock = System.Windows.Forms.DockStyle.Right;
-            this.voteLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.voteLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
-            this.voteLabel.Location = new System.Drawing.Point(1250, 1);
-            this.voteLabel.Name = "voteLabel";
-            this.voteLabel.Size = new System.Drawing.Size(58, 34);
-            this.voteLabel.TabIndex = 2;
-            this.voteLabel.Text = "Java";
-            // 
-            // label7
-            // 
-            this.label7.BackColor = System.Drawing.Color.Transparent;
-            this.label7.Dock = System.Windows.Forms.DockStyle.Right;
-            this.label7.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
-            this.label7.Location = new System.Drawing.Point(1308, 1);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(41, 34);
-            this.label7.TabIndex = 1;
-            this.label7.Text = "C#";
-            // 
-            // panelBrowser
-            // 
-            this.panelBrowser.BackColor = System.Drawing.Color.Transparent;
-            this.panelBrowser.Controls.Add(this.webView21);
-            this.panelBrowser.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelBrowser.Location = new System.Drawing.Point(0, 36);
-            this.panelBrowser.Name = "panelBrowser";
-            this.panelBrowser.Size = new System.Drawing.Size(1350, 692);
-            this.panelBrowser.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // webView21
             // 
             this.webView21.AllowExternalDrop = true;
+            this.webView21.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.webView21.CreationProperties = null;
             this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
-            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Location = new System.Drawing.Point(-1, -93);
             this.webView21.Name = "webView21";
-            this.webView21.Size = new System.Drawing.Size(1350, 692);
+            this.webView21.Size = new System.Drawing.Size(1452, 776);
             this.webView21.TabIndex = 0;
             this.webView21.ZoomFactor = 1D;
+            this.webView21.Click += new System.EventHandler(this.webView21_Click);
+            // 
+            // panelBrowser
+            // 
+            this.panelBrowser.BackColor = System.Drawing.Color.Transparent;
+            this.panelBrowser.Controls.Add(this.panel1);
+            this.panelBrowser.Controls.Add(this.webView21);
+            this.panelBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBrowser.Location = new System.Drawing.Point(0, 0);
+            this.panelBrowser.Name = "panelBrowser";
+            this.panelBrowser.Size = new System.Drawing.Size(1291, 729);
+            this.panelBrowser.TabIndex = 3;
+            this.panelBrowser.Paint += new System.Windows.Forms.PaintEventHandler(this.panelBrowser_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.panel1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.panel1.FillColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.panel1.FillColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.panel1.Location = new System.Drawing.Point(0, 670);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1291, 59);
+            this.panel1.TabIndex = 1;
+            // 
+            // voteLabel
+            // 
+            this.voteLabel.BackColor = System.Drawing.Color.Transparent;
+            this.voteLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.voteLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.voteLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.voteLabel.Location = new System.Drawing.Point(1, 137);
+            this.voteLabel.Name = "voteLabel";
+            this.voteLabel.Size = new System.Drawing.Size(57, 34);
+            this.voteLabel.TabIndex = 7;
+            this.voteLabel.Text = "Java";
             // 
             // FormCompiler
             // 
@@ -165,12 +173,13 @@
             this.ClientSize = new System.Drawing.Size(1350, 729);
             this.Controls.Add(this.panelBrowser);
             this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormCompiler";
             this.Text = "FormCompiler";
             this.Load += new System.EventHandler(this.FormCompiler_Load);
             this.guna2Panel1.ResumeLayout(false);
-            this.panelBrowser.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.panelBrowser.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -178,12 +187,12 @@
         #endregion
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label voteLabel;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panelBrowser;
         private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Panel panelBrowser;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel panel1;
+        private System.Windows.Forms.Label voteLabel;
     }
 }
--- FormCompiler.cs
+++ FormCompiler.cs
@@ -9,35 +9,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using System.IO;
+
 
 namespace Coder_s_space
 {
     public partial class FormCompiler : Form
     {
-        ChromiumWebBrowser browser;
-
+        //ChromiumWebBrowser browser;
         public FormCompiler()
         {
             InitializeComponent();
-            var settings = new CefSettings();
+            InitWebView();
+            /*var settings = new CefSettings();
             settings.PersistSessionCookies = true;
 
             Cef.Initialize(settings);
 
              browser = new ChromiumWebBrowser("https://www.facebook.com");
             browser.Dock = DockStyle.Fill;
-            panelBrowser.Controls.Add(browser); 
+            panelBrowser.Controls.Add(browser); */
 
-
-
-        }
-
-        private void FormCompiler_Load(object sender, EventArgs e)
-        {
             
+
+
+
+
+
         }
+
+        private async void FormCompiler_Load(object sender, EventArgs e)
+        {
+           
+        }
+       async void InitWebView()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.Navigate("https://www.tutorialspoint.com/compile_csharp_online.php");
+
+        }
+
+
 
         private void guna2CustomGradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void webView21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void voteLabel_Click(object sender, EventArgs e)
+        {
+            webView21.CoreWebView2.Navigate("https://www.tutorialspoint.com/compile_cpp_online.php");
+
+        }
+
+        private void panelBrowser_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2CustomGradientPanel1_Paint_1(object sender, PaintEventArgs e)
         {
 
         }
